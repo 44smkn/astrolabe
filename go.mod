705cf3bd3f5151ecb168a250c4a035b4f84d2465
Kenji Shimizu <jrywm121@gmail.com>
2020-12-14 22:10:36 +0900

    use xerrors instead of pkg/errors

--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 	golang.org/x/net v0.0.0-20201202161906-c7110b5ffcbb // indirect
 	golang.org/x/oauth2 v0.0.0-20201203001011-0b49973bad19
 	golang.org/x/time v0.0.0-20200630173020-3af7569d3a1e // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1
 	gopkg.in/yaml.v2 v2.4.0
 	k8s.io/api v0.19.4
 	k8s.io/apimachinery v0.19.4
--- parse.go
+++ parse.go
@@ -6,17 +6,28 @@ import (
 	"io/ioutil"
 	"os"
 
+	"golang.org/x/xerrors"
 	"gopkg.in/yaml.v2"
 )
 
+type NoExistFileError struct {
+	filepath string
+}
+
+func (e *NoExistFileError) Error() string {
+	return fmt.Sprintf("%s does not exist\n", e.filepath)
+}
+
 func DeserializeYamlFile(filepath string, out interface{}) error {
 	if _, err := os.Stat(filepath); os.IsNotExist(err) {
-		return fmt.Errorf("%s does not exist\n", filepath)
+		return xerrors.Errorf("%w", &NoExistFileError{
+			filepath: filepath,
+		})
 	}
 
 	data, err := ioutil.ReadFile(filepath)
 	if err != nil {
-		return fmt.Errorf("failed to read file. %v\n", err)
+		return xerrors.Errorf("%w", err)
 	}
 
 	decoder := yaml.NewDecoder(bytes.NewReader(data))
